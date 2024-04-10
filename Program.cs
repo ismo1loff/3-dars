e83967e03a9d9b1876c32b1def92069401e24f0a
Azizbey <azizbekismoiljonovtatu@gmail.com>
2024-04-10 09:19:20 +0500

    3-dars

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string firstInput = Console.ReadLine();
+string secondInput = Console.ReadLine();
+
+int firstNumber = Convert.ToInt32(firstInput);
+int secondNumber = Convert.ToInt32(secondInput);
+
+Console.WriteLine(firstNumber + secondNumber);
